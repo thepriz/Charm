75851d99852642f030363d7567dfcf6b1c615735
Sven Hjól <sven.hjol@protonmail.com>
2020-07-23 23:17:24 +0000

    Added FeatherFallingCrops

---
+++ src/main/java/svenhjol/charm/module/FeatherFallingCrops.java
@@ -0,0 +1,23 @@
+package svenhjol.charm.module;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.helper.EnchantmentsHelper;
+import svenhjol.meson.iface.Module;
+
+public class FeatherFallingCrops extends MesonModule {
+    @Module(description = "A player wearing feather falling enchanted boots will not trample crops.", hasSubscriptions = true)
+    public FeatherFallingCrops() {}
+
+    @SubscribeEvent
+    public void onFarmlandTrample(BlockEvent.FarmlandTrampleEvent event) {
+        if (event.getEntity() instanceof LivingEntity) {
+            LivingEntity entity = (LivingEntity)event.getEntity();
+            if (EnchantmentsHelper.hasFeatherFalling(entity)) {
+                event.setCanceled(true);
+            }
+        }
+    }
+}
