0ef24d5f69cc7b33426f624a13ada33223a9c0bf
Sven Hjól <sven.hjol@protonmail.com>
2020-08-04 06:00:56 +0000

    Don't set registry names early

--- src/main/java/svenhjol/charm/base/CharmSounds.java
+++ src/main/java/svenhjol/charm/base/CharmSounds.java
@@ -5,23 +5,23 @@ import net.minecraft.util.SoundEvent;
 import svenhjol.charm.Charm;
 import svenhjol.meson.MesonMod;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CharmSounds {
-    private static final List<SoundEvent> REGISTER = new ArrayList<>();
+    private static final Map<ResourceLocation, SoundEvent> REGISTER = new HashMap<>();
 
     public static final SoundEvent BOOKSHELF_OPEN = createSound("bookshelf_open");
     public static final SoundEvent BOOKSHELF_CLOSE = createSound("bookshelf_close");
 
     public static SoundEvent createSound(String name) {
         ResourceLocation res = new ResourceLocation(Charm.MOD_ID, name);
-        SoundEvent sound = new SoundEvent(res).setRegistryName(res);
-        REGISTER.add(sound);
+        SoundEvent sound = new SoundEvent(res);
+        REGISTER.put(res, sound);
         return sound;
     }
 
     public static void init(MesonMod mod) {
-        REGISTER.forEach(sound -> mod.register(sound, sound.getRegistryName()));
+        REGISTER.forEach((res, sound) -> mod.register(sound, res));
     }
 }
