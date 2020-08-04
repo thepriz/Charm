9514017576609021b6544a9927a5e596abb0f8bd
Sven Hjól <sven.hjol@protonmail.com>
2020-08-04 05:59:54 +0000

    Register blockitems to be the same as their parent blocks

--- src/main/java/svenhjol/meson/block/MesonBlockItem.java
+++ src/main/java/svenhjol/meson/block/MesonBlockItem.java
@@ -7,8 +7,11 @@ import net.minecraft.item.ItemStack;
 public class MesonBlockItem extends BlockItem {
     private int burnTime;
 
-    public MesonBlockItem(Block blockIn, Properties builder) {
-        super(blockIn, builder);
+    public MesonBlockItem(Block blockIn, Properties props) {
+        super(blockIn, props);
+
+        if (blockIn.getRegistryName() != null)
+            this.setRegistryName(blockIn.getRegistryName());
     }
 
     public void setBurnTime(int burnTime) {
