6b3d737d1dc637ba2a5666614f8de739e115533f
Sven Hjól <sven.hjol@protonmail.com>
2020-08-04 06:04:24 +0000

    Restore all vanilla types

--- src/main/java/svenhjol/charm/module/VariantChests.java
+++ src/main/java/svenhjol/charm/module/VariantChests.java
@@ -36,15 +36,16 @@ public class VariantChests extends MesonModule {
 
     @Override
     public void init() {
-        VanillaStorageMaterial type = VanillaStorageMaterial.SPRUCE;
-        NORMAL_CHEST_BLOCKS.put(type, new VariantChestBlock(this, type));
-        TRAPPED_CHEST_BLOCKS.put(type, new VariantTrappedChestBlock(this, type));
+        for (VanillaStorageMaterial type : VanillaStorageMaterial.values()) {
+            NORMAL_CHEST_BLOCKS.put(type, new VariantChestBlock(this, type));
+            TRAPPED_CHEST_BLOCKS.put(type, new VariantTrappedChestBlock(this, type));
 
-        NORMAL_TILE = TileEntityType.Builder.create(VariantChestTileEntity::new, NORMAL_CHEST_BLOCKS.values().toArray(new Block[0])).build(null);
-        TRAPPED_TILE = TileEntityType.Builder.create(VariantTrappedChestTileEntity::new, TRAPPED_CHEST_BLOCKS.values().toArray(new Block[0])).build(null);
+            NORMAL_TILE = TileEntityType.Builder.create(VariantChestTileEntity::new, NORMAL_CHEST_BLOCKS.values().toArray(new Block[0])).build(null);
+            TRAPPED_TILE = TileEntityType.Builder.create(VariantTrappedChestTileEntity::new, TRAPPED_CHEST_BLOCKS.values().toArray(new Block[0])).build(null);
 
-        mod.register(NORMAL_TILE, NORMAL_ID);
-        mod.register(TRAPPED_TILE, TRAPPED_ID);
+            mod.register(NORMAL_TILE, NORMAL_ID);
+            mod.register(TRAPPED_TILE, TRAPPED_ID);
+        }
     }
 
     @Override
