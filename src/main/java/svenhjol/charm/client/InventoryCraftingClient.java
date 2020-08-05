38a3fc217e79b0ab9ef3fc3c2fd45ca45bf5508e
Sven Hjól <sven.hjol@protonmail.com>
2020-08-05 00:27:14 +0000

    Fixes for crafting client

--- src/main/java/svenhjol/charm/client/InventoryCraftingClient.java
+++ src/main/java/svenhjol/charm/client/InventoryCraftingClient.java
@@ -2,6 +2,7 @@ package svenhjol.charm.client;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.inventory.CreativeScreen;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.client.gui.widget.button.ImageButton;
 import net.minecraft.item.ItemStack;
@@ -14,10 +15,32 @@ import svenhjol.meson.MesonModule;
 
 public class InventoryCraftingClient {
     private final MesonModule module;
-    private ImageButton craftingButton;
+    public ImageButton craftingButton;
 
     public InventoryCraftingClient(MesonModule module) {
         this.module = module;
+    }
+
+    @SubscribeEvent
+    public void onInitGui(InitGuiEvent.Post event) {
+        Minecraft mc = Minecraft.getInstance();
+
+        if (mc.player == null)
+            return;
+
+        if (!(event.getGui() instanceof InventoryScreen) || event.getGui() instanceof CreativeScreen)
+            return;
+
+        InventoryScreen screen = (InventoryScreen)event.getGui();
+
+        this.craftingButton = new ImageButton(screen.getGuiLeft() + 120, screen.height / 2 - 22, 20, 18, 0, 0, 19, CharmResources.INVENTORY_BUTTONS, click -> {
+            this.module.mod.getPacketHandler().sendToServer(new ServerOpenCrafting());
+        });
+
+        if (!mc.player.inventory.hasItemStack(new ItemStack(Blocks.CRAFTING_TABLE)))
+            craftingButton.visible = false;
+
+        event.addWidget(this.craftingButton);
     }
 
     @SubscribeEvent
@@ -34,39 +57,9 @@ public class InventoryCraftingClient {
 
         if (mc.player.world.getGameTime() % 5 == 0)
             craftingButton.visible = mc.player.inventory.hasItemStack(new ItemStack(Blocks.CRAFTING_TABLE));
-
-        InventoryScreen screen = (InventoryScreen)event.getGuiContainer();
-        craftingButton.setPosition(screen.getGuiLeft() + 130, screen.height / 2 - 22);
     }
 
-    @SubscribeEvent
-    public void onInitGui(InitGuiEvent.Post event) {
-        Minecraft mc = Minecraft.getInstance();
-
-        if (mc.player == null)
-            return;
-
-        if (!(event.getGui() instanceof InventoryScreen))
-            return;
-
-        InventoryScreen screen = (InventoryScreen)event.getGui();
-
-        this.craftingButton = new ImageButton(screen.getGuiLeft() + 130, screen.height / 2 - 22, 20, 18, 0, 0, 19, CharmResources.INVENTORY_BUTTONS, click -> {
-            this.module.mod.getPacketHandler().sendToServer(new ServerOpenCrafting());
-        });
-
-        if (!mc.player.inventory.hasItemStack(new ItemStack(Blocks.CRAFTING_TABLE)))
-            craftingButton.visible = false;
-
-        event.addWidget(this.craftingButton);
-    }
-
-    public void openInventory() {
-        Minecraft mc = Minecraft.getInstance();
-
-        if (mc.player == null)
-            return;
-
-        mc.displayGuiScreen(new InventoryScreen(mc.player));
+    public boolean isButtonVisible() {
+        return this.craftingButton != null && this.craftingButton.visible;
     }
 }
