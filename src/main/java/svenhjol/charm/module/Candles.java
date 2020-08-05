7558eb8e3fb6e71a6787914392392aa0ceaa961b
Sven Hjól <sven.hjol@protonmail.com>
2020-08-05 01:46:55 +0000

    Configurable light and placement for candles

--- src/main/java/svenhjol/charm/module/Candles.java
+++ src/main/java/svenhjol/charm/module/Candles.java
@@ -11,10 +11,16 @@ public class Candles extends MesonModule {
     public static CandleBlock CANDLE;
     public static BeeswaxItem BEESWAX;
 
+    @Config(name = "Light level", description = "The amount of light (out of 15) that a lit candle will provide.")
+    public static int lightLevel = 9;
+
+    @Config(name = "Lit when placed", description = "If true, candles will be lit when placed rather than needing a flint and steel.")
+    public static boolean litWhenPlaced = false;
+
     @Config(name = "Override", description = "This module is automatically disabled if Quark is present. Set true to force enable.")
     public static boolean override = false;
 
-    @Module(description = "Candles have a lower ambient light than torches and are made from beeswax.")
+    @Module(description = "Candles are made from beeswax and have a lower ambient light than torches.")
     public Candles() {}
 
     @Override
