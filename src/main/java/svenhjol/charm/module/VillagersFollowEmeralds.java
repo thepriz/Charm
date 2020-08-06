a91e7805872e8d030f1a98b7aea6b72c3fee5186
Sven Hjól <sven.hjol@protonmail.com>
2020-08-06 19:50:13 +0000

    Just emerald blocks

--- src/main/java/svenhjol/charm/module/VillagersFollowEmeralds.java
+++ src/main/java/svenhjol/charm/module/VillagersFollowEmeralds.java
@@ -4,7 +4,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.TemptGoal;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
-import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,13 +14,10 @@ import svenhjol.meson.iface.Module;
 import svenhjol.meson.mixin.GoalSelectorAccessor;
 
 public class VillagersFollowEmeralds extends MesonModule {
-    @Config(name = "Only attracted to block", description = "If true, villagers will only be attracted to a block of emerald.")
-    public static boolean onlyBlock = false;
-
     @Config(name = "Override", description = "This module is automatically disabled if Quark is present. Set true to force enable.")
     public static boolean override = false;
 
-    @Module(description = "Villagers are attracted when the player holds an emerald or block of emeralds.", hasSubscriptions = true)
+    @Module(description = "Villagers are attracted when the player holds a block of emeralds.", hasSubscriptions = true)
     public VillagersFollowEmeralds() {}
 
     @Override
@@ -39,7 +35,7 @@ public class VillagersFollowEmeralds extends MesonModule {
         if (entity instanceof VillagerEntity) {
             VillagerEntity villager = (VillagerEntity) entity;
 
-            Ingredient ingredient = onlyBlock ? Ingredient.fromItems(Blocks.EMERALD_BLOCK) : Ingredient.fromItems(Blocks.EMERALD_BLOCK, Items.EMERALD);
+            Ingredient ingredient = Ingredient.fromItems(Blocks.EMERALD_BLOCK);
 
             if (((GoalSelectorAccessor)villager.goalSelector).getGoals().stream().noneMatch(g -> g.getGoal() instanceof TemptGoal))
                 villager.goalSelector.addGoal(3, new TemptGoal(villager, 0.6, ingredient, false));
