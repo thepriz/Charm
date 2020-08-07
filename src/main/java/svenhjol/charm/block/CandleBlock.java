92a4afa04c4dde327f9398b430c199fd3bfaeace
Sven Hjól <sven.hjol@protonmail.com>
2020-08-07 04:25:23 +0000

    Fun times with candle waterlogging

--- src/main/java/svenhjol/charm/block/CandleBlock.java
+++ src/main/java/svenhjol/charm/block/CandleBlock.java
@@ -3,6 +3,9 @@ package svenhjol.charm.block;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particles.IParticleData;
@@ -10,9 +13,7 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -27,12 +28,14 @@ import svenhjol.charm.module.Candles;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.block.MesonBlock;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
-public class CandleBlock extends MesonBlock {
+public class CandleBlock extends MesonBlock implements IWaterLoggable {
     protected static final VoxelShape SHAPE = Block.makeCuboidShape(6.0D, 0.0D, 6.0D, 10.0D, 9.0D, 10.0D);
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     private final IParticleData flame;
-    public static BooleanProperty LIT = BlockStateProperties.LIT;
 
     public CandleBlock(MesonModule module) {
         super(module, "candle", AbstractBlock.Properties
@@ -42,25 +45,54 @@ public class CandleBlock extends MesonBlock {
             .hardnessAndResistance(0.5F));
 
         this.flame = ParticleTypes.FLAME;
-        this.setDefaultState(getDefaultState().with(LIT, Candles.litWhenPlaced));
+        this.setDefaultState(getDefaultState().with(LIT, Candles.litWhenPlaced).with(WATERLOGGED, false));
     }
 
+    @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+        boolean flag = fluidstate.getFluid() == Fluids.WATER;
+        return super.getStateForPlacement(context).with(WATERLOGGED, Boolean.valueOf(flag));
     }
 
     @Override
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         ItemStack held = player.getHeldItem(handIn);
-        if (held.getItem() == Items.FLINT_AND_STEEL && !state.get(LIT)) {
+        if (held.getItem() == Items.FLINT_AND_STEEL
+            && !state.get(LIT)
+            && !state.get(WATERLOGGED)
+        ) {
             worldIn.setBlockState(pos, state.with(LIT, true));
             return ActionResultType.SUCCESS;
         }
         return ActionResultType.PASS;
     }
 
+    @Override
     public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-        return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+        if (stateIn.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+
+        return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos)
+            ? Blocks.AIR.getDefaultState()
+            : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    @Override
+    public boolean receiveFluid(IWorld worldIn, BlockPos pos, BlockState state, FluidState fluidStateIn) {
+        if (!state.get(BlockStateProperties.WATERLOGGED) && fluidStateIn.getFluid() == Fluids.WATER) {
+            worldIn.setBlockState(pos, state.with(WATERLOGGED, true).with(LIT, false), 3);
+            worldIn.getPendingFluidTicks().scheduleTick(pos, fluidStateIn.getFluid(), fluidStateIn.getFluid().getTickRate(worldIn));
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
@@ -68,19 +100,26 @@ public class CandleBlock extends MesonBlock {
         return Block.hasEnoughSolidSide(worldIn, pos.down(), Direction.UP);
     }
 
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
     @OnlyIn(Dist.CLIENT)
     public void animateTick(BlockState state, World world, BlockPos pos, Random rand) {
-        if (state.get(CandleBlock.LIT)) {
+        if (state.get(CandleBlock.LIT) && !state.get(CandleBlock.WATERLOGGED)) {
             double d0 = (double)pos.getX() + 0.48D;
             double d1 = (double)pos.getY() + 0.68D;
             double d2 = (double)pos.getZ() + 0.48D;
-            world.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            if (rand.nextFloat() < 0.25F)
+                world.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+
             world.addParticle(this.flame, d0, d1, d2, 0.0D, 0.0D, 0.0D);
         }
     }
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(LIT);
+        builder.add(LIT, WATERLOGGED);
     }
 }
