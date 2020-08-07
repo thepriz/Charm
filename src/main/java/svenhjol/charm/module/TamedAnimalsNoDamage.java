9b0464de263d12ba9b1e4fae79b3e8f6ac13981c
Sven Hjól <sven.hjol@protonmail.com>
2020-08-07 23:45:06 +0000

    Reorganise method

--- src/main/java/svenhjol/charm/module/TamedAnimalsNoDamage.java
+++ src/main/java/svenhjol/charm/module/TamedAnimalsNoDamage.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,22 +29,26 @@ public class TamedAnimalsNoDamage extends MesonModule {
 
     @SubscribeEvent
     public void onLivingHurt(LivingHurtEvent event) {
-        if (!event.isCanceled()
-            && (!(event.getEntityLiving() instanceof PlayerEntity))
-        ) {
-            Entity attacker = event.getSource().getImmediateSource();
-            Entity source = event.getSource().getTrueSource();
+        if (!event.isCanceled()) {
+            if (tryIgnoreDamage(event.getEntityLiving(), event.getSource()))
+                event.setCanceled(true);
+        }
+    }
+
+    private boolean tryIgnoreDamage(LivingEntity entity, DamageSource damageSource) {
+        if (!(entity instanceof PlayerEntity)) {
+            Entity attacker = damageSource.getImmediateSource();
+            Entity source = damageSource.getTrueSource();
+
             PlayerEntity player = null;
 
             if (source instanceof PlayerEntity) player = (PlayerEntity) source;
             if (attacker instanceof PlayerEntity) player = (PlayerEntity) attacker;
 
             if (player != null && !player.isCreative()) {
-                LivingEntity target = event.getEntityLiving();
-                
-                if (target instanceof TameableEntity && ((TameableEntity) target).isTamed())
-                    event.setCanceled(true);
+                return (entity instanceof TameableEntity && ((TameableEntity) entity).isTamed());
             }
         }
+        return false;
     }
 }
