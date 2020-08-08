8202ca1a7077b3c47a62744e45ff3370aab7ec3b
Sven Hjól <sven.hjol@protonmail.com>
2020-08-08 00:46:05 +0000

    Add config to make soul campfires keep damage

--- src/main/java/svenhjol/charm/mixin/CampfiresNoDamageMixin.java
+++ src/main/java/svenhjol/charm/mixin/CampfiresNoDamageMixin.java
@@ -1,6 +1,7 @@
 package svenhjol.charm.mixin;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.module.CampfiresNoDamage;
 import svenhjol.meson.Meson;
 
 @Mixin(CampfireBlock.class)
@@ -19,7 +21,9 @@ public abstract class CampfiresNoDamageMixin {
         cancellable = true
     )
     private void onEntityCollisionHook(BlockState state, World worldIn, BlockPos pos, Entity entityIn, CallbackInfo ci) {
-        if (Meson.enabled("charm:campfires_no_damage"))
-            ci.cancel();
+        if (Meson.enabled("charm:campfires_no_damage")) {
+            if (!(CampfiresNoDamage.soulCampfiresDamage && state.getBlock() == Blocks.SOUL_CAMPFIRE))
+                ci.cancel();
+        }
     }
 }
--- src/main/java/svenhjol/charm/module/CampfiresNoDamage.java
+++ src/main/java/svenhjol/charm/module/CampfiresNoDamage.java
@@ -1,9 +1,13 @@
 package svenhjol.charm.module;
 
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.iface.Config;
 import svenhjol.meson.iface.Module;
 
 public class CampfiresNoDamage extends MesonModule {
+    @Config(name = "Soul campfires cause damage", description = "If true, soul campfires will still cause damage to mobs.")
+    public static boolean soulCampfiresDamage = true;
+
     @Module(description = "Campfires no longer damage mobs.")
     public CampfiresNoDamage() {}
 }
