60dfb5f4b03639770ace132d592a68fa5908e26c
Sven Hjól <sven.hjol@protonmail.com>
2020-08-08 02:57:01 +0000

    Added basic cache to ModHelper lookup

--- src/main/java/svenhjol/meson/helper/ModHelper.java
+++ src/main/java/svenhjol/meson/helper/ModHelper.java
@@ -2,15 +2,29 @@ package svenhjol.meson.helper;
 
 import net.minecraftforge.fml.ModList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModHelper {
+    private static List<String> enabled = new ArrayList<>();
+
     public static boolean present(String... mods) {
-        boolean loaded = false;
+        // basic cache
+        for (String mod : mods) {
+            if (enabled.contains(mod))
+                return true;
+        }
+
         ModList modList = ModList.get();
 
         for (String mod : mods) {
-            loaded = loaded || modList.isLoaded(mod);
+            if (modList.isLoaded(mod)) {
+                enabled.add(mod);
+            } else {
+                return false;
+            }
         }
 
-        return loaded;
+        return true;
     }
 }
