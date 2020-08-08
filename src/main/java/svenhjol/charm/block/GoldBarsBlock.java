f41ab67b071fb915a7503056ee78ad66f3f724a8
Sven Hjól <sven.hjol@protonmail.com>
2020-08-08 01:12:14 +0000

    Gold bars properly respects quark

--- src/main/java/svenhjol/charm/block/GoldBarsBlock.java
+++ src/main/java/svenhjol/charm/block/GoldBarsBlock.java
@@ -3,6 +3,8 @@ package svenhjol.charm.block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.PaneBlock;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.block.IMesonBlock;
 
@@ -18,6 +20,12 @@ public class GoldBarsBlock extends PaneBlock implements IMesonBlock {
     @Override
     public ItemGroup getItemGroup() {
         return ItemGroup.DECORATIONS;
+    }
+
+    @Override
+    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+        if (enabled())
+            super.fillItemGroup(group, items);
     }
 
     @Override
