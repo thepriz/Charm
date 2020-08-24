63cac070916b7f04066f7fb5a6bf888710c77d34
Sven Hjól <sven.hjol@protonmail.com>
2020-08-24 16:57:02 +0000

    Revert renderer

--- src/main/java/svenhjol/charm/render/VariantSheepRenderer.java
+++ src/main/java/svenhjol/charm/render/VariantSheepRenderer.java
@@ -1,19 +1,15 @@
 package svenhjol.charm.render;
 
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.SheepRenderer;
-import net.minecraft.client.renderer.entity.layers.SheepWoolLayer;
-import net.minecraft.client.renderer.entity.model.SheepModel;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import svenhjol.charm.module.VariantAnimalTextures;
 
-public class VariantSheepRenderer extends MobRenderer<SheepEntity, SheepModel<SheepEntity>> {
+public class VariantSheepRenderer extends SheepRenderer {
     public VariantSheepRenderer(EntityRendererManager manager) {
-        super(manager, new SheepModel<>(), 0.7F);
-        this.addLayer(new SheepWoolLayer(this));
+        super(manager);
     }
 
     @SuppressWarnings("rawtypes")
