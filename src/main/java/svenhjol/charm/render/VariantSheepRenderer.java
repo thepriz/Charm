d30283823367d07034473463ee272b702ff7127f
Sven Hjól <sven.hjol@protonmail.com>
2020-08-24 15:46:40 +0000

    Reimplement wool layer

--- src/main/java/svenhjol/charm/render/VariantSheepRenderer.java
+++ src/main/java/svenhjol/charm/render/VariantSheepRenderer.java
@@ -1,15 +1,19 @@
 package svenhjol.charm.render;
 
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.SheepRenderer;
+import net.minecraft.client.renderer.entity.layers.SheepWoolLayer;
+import net.minecraft.client.renderer.entity.model.SheepModel;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import svenhjol.charm.module.VariantAnimalTextures;
 
-public class VariantSheepRenderer extends SheepRenderer {
+public class VariantSheepRenderer extends MobRenderer<SheepEntity, SheepModel<SheepEntity>> {
     public VariantSheepRenderer(EntityRendererManager manager) {
-        super(manager);
+        super(manager, new SheepModel<>(), 0.7F);
+        this.addLayer(new SheepWoolLayer(this));
     }
 
     @SuppressWarnings("rawtypes")
